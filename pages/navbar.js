4c825ee18cdf299d699fb330e1e6d75eb62fa769
sugyo <sg.guru1030@gmail.com>
2022-10-18 22:33:36 +1000

    commit2

--- pages/navbar.js
+++ pages/navbar.js
@@ -1,101 +1,76 @@
-import Head from 'next/head';
 import Link from 'next/link';
 import { useState } from 'react';
+// import { navLinks } from "../config/navbar.config";
 
-
-export default function Home() {
-  const [navbar, setNavbar] = useState(false);
+export default function Navbar() {
+  const [active, setActive] = useState(false);
+  const handleClick = () => {
+    setActive(!active);
+  };
   return (
-    <div>
-      <Head>
-        <title>Create Next Responsive Navbar With Tailwind CSS</title>
-        <meta
-          name="description"
-          content="Create Next JS Responsive Menu with Tailwind CSS"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <nav className="w-full bg-gray-800 shadow">
-        <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
-          <div>
-            <div className="flex items-center justify-between py-3 md:py-5 md:block">
-              <a href="#">
-                <h2 className="text-2xl text-white font-bold">NEXT JS</h2>
-              </a>
-              <div className="md:hidden">
-                <button
-                  className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
-                  onClick={() => setNavbar(!navbar)}
-                >
-                  {navbar ? (
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="w-6 h-6 text-white"
-                      viewBox="0 0 20 20"
-                      fill="currentColor"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                        clipRule="evenodd"
-                      />
-                    </svg>
-                  ) : (
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="w-6 h-6 text-white"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                      strokeWidth={2}
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M4 6h16M4 12h16M4 18h16"
-                      />
-                    </svg>
-                  )}
-                </button>
-              </div>
-            </div>
-          </div>
-          <div>
-            <div
-              className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${navbar ? 'block' : 'hidden'
-                }`}
+    <>
+      <nav className='flex items-center flex-wrap bg-green-400 p-3 ' style={{ backgroundImage: '' }}>
+        <Link href='/'>
+          <a className='inline-flex items-center p-2 mr-4 '>
+            <svg
+              viewBox='0 0 24 24'
+              xmlns='http://www.w3.org/2000/svg'
+              className='fill-current text-white h-8 w-8 mr-2'
             >
-              <ul className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
-                <li className="text-white">
-                  <Link href="/">
-                    <a>Home</a>
-                  </Link>
-                </li>
-                <li className="text-white">
-                  <Link href="/blogs">
-                    <a>Blogs</a>
-                  </Link>
-                </li>
-                <li className="text-white">
-                  <Link href="/about">
-                    <a>About US</a>
-                  </Link>
-                </li>
-                <li className="text-white">
-                  <Link href="/contact">
-                    <a>Contact US</a>
-                  </Link>
-                </li>
-              </ul>
-            </div>
+              <path d='M12.001 4.8c-3.2 0-5.2 1.6-6 4.8 1.2-1.6 2.6-2.2 4.2-1.8.913.228 1.565.89 2.288 1.624C13.666 10.618 15.027 12 18.001 12c3.2 0 5.2-1.6 6-4.8-1.2 1.6-2.6 2.2-4.2 1.8-.913-.228-1.565-.89-2.288-1.624C16.337 6.182 14.976 4.8 12.001 4.8zm-6 7.2c-3.2 0-5.2 1.6-6 4.8 1.2-1.6 2.6-2.2 4.2-1.8.913.228 1.565.89 2.288 1.624 1.177 1.194 2.538 2.576 5.512 2.576 3.2 0 5.2-1.6 6-4.8-1.2 1.6-2.6 2.2-4.2 1.8-.913-.228-1.565-.89-2.288-1.624C10.337 13.382 8.976 12 6.001 12z' />
+            </svg>
+            <span className='text-xl text-white font-bold uppercase tracking-wide'>
+              Talwind CSS
+            </span>
+          </a>
+        </Link>
+        <button
+          className=' inline-flex p-3 hover:bg-green-600 rounded lg:hidden text-white ml-auto hover:text-white outline-none'
+        >
+          <svg
+            className='w-6 h-6'
+            fill='none'
+            stroke='currentColor'
+            viewBox='0 0 24 24'
+            xmlns='http://www.w3.org/2000/svg'
+          >
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              strokeWidth={2}
+              d='M4 6h16M4 12h16M4 18h16'
+            />
+          </svg>
+        </button>
+        {/*Note that in this div we will use a ternary operator to decide whether or not to display the content of the div  */}
+        <div
+          className={`${active ? '' : 'hidden'
+            }   w-full lg:inline-flex lg:flex-grow lg:w-auto`}
+        >
+          <div className='lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start  flex flex-col lg:h-auto'>
+            <Link href='/'>
+              <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-green-600 hover:text-white '>
+                Home
+              </a>
+            </Link>
+            <Link href='/'>
+              <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-green-600 hover:text-white'>
+                Services
+              </a>
+            </Link>
+            <Link href='/'>
+              <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-green-600 hover:text-white'>
+                About us
+              </a>
+            </Link>
+            <Link href='/'>
+              <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-green-600 hover:text-white'>
+                Contact us
+              </a>
+            </Link>
           </div>
         </div>
       </nav>
-      <div className="flex justify-center items-center mt-8">
-        <h1 className="text-2xl font-bold text-purple-500">
-          Create Responsive Navbar Menu in Next js with Tailwind CSS
-        </h1>
-      </div>
-    </div>
+    </>
   );
 }
--- pages/test.js
+++ pages/test.js
@@ -1,9 +1,25 @@
-import Styleguide from '../components/styleguide';
+import styles from '../styles/Test.module.css'
 
 export default function Test() {
-    return (
-        <div>
-            <Styleguide />
-        </div>
-    )
+  return (
+    <div class={styles.styleguide}>
+      <p className={styles.text}>Switch</p>
+      <label class={styles.switch}>
+        <input type="checkbox" />
+        <span class={styles.switchslider}></span>
+        <span class={styles.switch_back}></span>
+      </label>
+      <p className={styles.text}>Slider</p>
+      <div class="slidecontainer">
+        <input
+          type="range"
+          min="1"
+          max="100"
+          value="50"
+          class={styles.slider}
+          id="myRange"
+        />
+      </div>
+    </div>
+  )
 }
